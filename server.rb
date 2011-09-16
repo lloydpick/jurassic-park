fb8850351dbb50641f1fdbbf0a89010649ecf7ba
Lloyd Pick <lloydpick@gmail.com>
2011-09-17 00:22:31 +0100

    new tab urls and cleanup

--- server.rb
+++ server.rb
@@ -9,13 +9,11 @@ get '/' do
   haml :index
 end
 
+# TABS
+get '/tab1' do haml :tab1 end
+get '/tab2' do haml :tab2 end
+get '/tab3' do haml :tab3 end
 
 # STYLESHEETS
-
-get '/stylesheet.css' do
-  sass :stylesheet
-end
-
-get '/map.css' do
-  sass :map
-end
+get '/stylesheet.css' do sass :stylesheet end
+get '/map.css' do sass :map end
